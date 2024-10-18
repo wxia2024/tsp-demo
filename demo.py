5e3db4fbf879ad22806b593066d3676855eebca0
wxia2024 <wxia@mt.feitian.edu>
2024-10-18 04:31:36 -0400

    working python_tsp demo

--- demo.py
+++ demo.py
@@ -1,1 +1,13 @@
-print('this is better')
+import numpy as np
+from python_tsp.exact import solve_tsp_dynamic_programming
+
+distance_matrix = np.array([
+    [0,  5, 4, 10],
+    [5,  0, 8,  5],
+    [4,  8, 0,  3],
+    [10, 5, 3,  0]
+])
+permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
+
+print(permutation)
+print(distance)
